412512b2e329d04ecd81402e8878d7336ee159d8
Дмитрий Страхов <strahov@fittin.ru>
2020-04-04 03:53:38 +0300

    Немного комментариев

--- ChartingView/ChartingView/Form1.cs
+++ ChartingView/ChartingView/Form1.cs
@@ -16,8 +16,11 @@ namespace ChartingView
         public Form1()
         {
             InitializeComponent();
+            //Добавление точки данных
             chart1.Series[0].Points.AddY(20);
             chart1.Series[0].Points.AddY(20);
+
+            //Добавление подписи к 0 точке серии
             chart1.Series[0].Points[0].Label= "20";
         }
     }
