f5c6689333ec4e6ad15054847c16017f55a788da
Дмитрий Страхов <strahov@fittin.ru>
2020-04-04 03:51:49 +0300

    Добавил проект с примером

---
+++ ChartingView/ChartingView/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ChartingView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Series[0].Points.AddY(20);
+            chart1.Series[0].Points.AddY(20);
+            chart1.Series[0].Points[0].Label= "20";
+        }
+    }
+}
